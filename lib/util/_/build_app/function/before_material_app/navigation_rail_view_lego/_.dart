ac4a831e22336df2122f257b351cbfcbe9b9bc38
june lee <melodysdreamj@gmail.com>
2024-07-15 03:01:02 +0900

    Initial commit

---
+++ lib/util/_/build_app/function/before_material_app/navigation_rail_view_lego/_.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../main.dart';
+
+// @ReadyBeforeMaterialApp()
+// Future<void> readyForNavigationRailViewLego(BuildContext context) async {}
---
+++ lib/util/_/build_app/widget/my_app/navigation_rail_view_lego/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverMyApp()
+// Widget Function() coverNavigationRailViewLego(Widget Function() materialAppBuilder) {
+//   return () => materialAppBuilder();
+// }
